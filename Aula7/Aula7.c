60349eecb050b30fa15a6028fc4494e8775ffc1d
Élton Reisdorfer <eltonreisdorfer2@gmail.com>
2020-06-25 23:45:36 -0300

    Refatorações de códigos e implementação de lógica de final de jogo por pontuação e contagem das bombas.

--- Aula7/Aula7.c
+++ Aula7/Aula7.c
@@ -87,7 +87,7 @@ void MostraTabuleiro(int Tabuleiro[_Height_][_Width_],
 				case 6:
 				case 7:
 				case 8:  printf("  %d  ", Tabuleiro[L][C]); break;
-				//case _BANDEIRA_: printf("  %d  ", 15); break;
+				case _BANDEIRA_: printf("  %d  ", 63); break;
 				default: printf("  .  "); break;
 				}
 			}
@@ -177,97 +177,141 @@ void MontaTabuleiro(int Tabuleiro[_Height_][_Width_])
 
 }
 
-void Bandeira(int Tabuleiro[_Height_][_Width_], int TabAuxiliar[_Height_][_Width_], int l, int c) {
+// Função para inserir ou remover bandeira
+void Bandeira(int Tabuleiro[_Height_][_Width_], int TabAuxBandeiras[_Height_][_Width_], int l, int c) {
+	// Se não existe bandeira na linha e coluna, então adiciona
 	if (Tabuleiro[l][c] != _BANDEIRA_) {
-		TabAuxiliar[l][c] = Tabuleiro[l][c];
-		Tabuleiro[l][c] = _BANDEIRA_;
+		TabAuxBandeiras[l][c] = Tabuleiro[l][c]; // Faz bkp do conteudo original
+		Tabuleiro[l][c] = _BANDEIRA_; // Adiciona a bandeira
 		printf("Bandeira adicionada !\n");
 	}
+	// Se já existe a bandeira, então remove
 	else {
-		Tabuleiro[l][c] = TabAuxiliar[l][c];
-		TabAuxiliar[l][c] = _BANDEIRA_;
+		// Volta o bkp do tabuleiro de bandeiras
+		Tabuleiro[l][c] = TabAuxBandeiras[l][c];
+		// Por desencargo de consciência joga a bandeira pro auxiliar de bandeiras
+		TabAuxBandeiras[l][c] = _BANDEIRA_;
 		printf("Bandeira removida !\n");
 	}
 }
 
-int main()
-{
-
-	int Tabuleiro[_Height_][_Width_];
-	int TabVisual[_Height_][_Width_];
-	int TabAuxiliar[_Height_][_Width_];
-	// _FECHADA_ : -1 : celula fechada
-	// _ABERTA_  :  0 : celula aberta
-
+// Função para abertura ou fechamento de todas as células do tabuleiro
+void AbreFechaTabuleiro(int TabVisual[_Height_][_Width_], int AbertaFechada) {
 	int L, C;
-
-	//srand(time(NULL)); // inicia a semente do randomizador
-
-	// teste: abre tabuleiro
 	for (L = 0; L < _Height_; L++)
 	{
 		for (C = 0; C < _Width_; C++)
 		{
-			TabVisual[L][C] = _FECHADA_; // todas as células estao fechadas!
+			TabVisual[L][C] = AbertaFechada; // Abre ou fecha todas as células
 		}
 	}
+}
 
+int ContaBandeiras(int Tabuleiro[_Height_][_Width_]) {
+	int L, C, nBandeiras = 0;
+	for (L = 0; L < _Height_; L++)
+	{
+		for (C = 0; C < _Width_; C++)
+		{
+			if (Tabuleiro[L][C] == _BANDEIRA_) nBandeiras++; // bandeiras marcadas
+		}
+	}
+	return nBandeiras;
+}
+
+int main()
+{
+	// Declaramos todas as nossas estruturas
+	int Tabuleiro[_Height_][_Width_]; // Tabuleiro de bombas, contagens células vazias
+	int TabVisual[_Height_][_Width_]; // Tabuleiro de caracteres visuais
+	int TabAuxBandeiras[_Height_][_Width_]; // Tabuleiro de bandeiras
+
+	//srand(time(NULL)); // Iniciamos randomizador
+
+	// Mandamos fechar todo o tabuleiro de visual do jogo
+	AbreFechaTabuleiro(TabVisual, _FECHADA_);
+
+	// Montamos o tabuleiro
 	MontaTabuleiro(Tabuleiro);
-
+	// Inicializamos a pontuação
 	int pontos = 0;
 
+	// Mostramos o tabuleiro inicial
 	printf("Campo Minado:\n");
 	MostraTabuleiro(Tabuleiro, TabVisual);
 
+	// Laço infinito, enquanto não for quebrado
 	while (true) {
-
+		// Solicita para o usuário a ação a ser tomada
 		int op = 0;
-
 		printf("Para jogar, digite:\n");
-		printf("1 - Jogada normal, 2 - Inserir/Remover Bandeira\n");
+		printf("1 - Abir celula, 2 - Inserir/Remover Bandeira, 3 - Sair do jogo\n");
 		scanf_s("%d", &op);
 		// Valida a opção de jogada digitada pelo usuário
-		if (op < 1 || op >2) {
+		if (op < 1 || op >3) {
 			printf("Opcao invalida !\n");
 			continue;
 		}
-
+		// Ação de sair do jogo
+		if (op == 3) {
+			MostraTabuleiro(Tabuleiro, TabVisual);
+			printf("Final de jogo, voce saiu ! sua pontuacao: %d\n", pontos);
+			break;
+		}
+		// Solicita para o usuário a célula onde será efetuada a jogada
 		int l = 0, c = 0;
-
 		printf("Digite a linha desejada:\n");
 		scanf_s("%d", &l);
-
 		printf("Digite a coluna desejada:\n");
 		scanf_s("%d", &c);
-
+		// Valida a célula onde será realizada a jogada
 		if (l < 1 || c < 1 || l > _Height_ || c > _Width_) {
 			printf("Jogada em linha ou coluna invalidas ! L(%d) C(%d)\n", l, c);
 		}
 		else {
+			// Analisa o tipo de jogada a ser efetuada
 			if (op == 1) {
+				// Jogada normal, manda abrir a célula
 				int celulas = AbreArea(Tabuleiro, TabVisual, c - 1, l - 1, false);
+				// Se encontrar uma bomba finaliza o jogo
 				if (celulas == _BOMBA_)
 				{
+					// Mandamos abrir todo o tabuleiro para mostrar as bombas
+					AbreFechaTabuleiro(TabVisual, _ABERTA_);
 					printf("Campo Minado:\n");
+					// Mandamos exibir o tabuleiro
 					MostraTabuleiro(Tabuleiro, TabVisual);
 					printf("Final de jogo ! Voce abriu uma bomba, sua pontuacao: 0\n");
 					break;
 				}
+				// Incrementa nossos pontos
 				pontos += celulas;
 			}
 			else {
-				Bandeira(Tabuleiro, TabAuxiliar, l - 1, c - 1);
+				// Adicionar ou remover bandeira
+				Bandeira(Tabuleiro, TabAuxBandeiras, l - 1, c - 1);
 			}
+			// Faz a contagem das bandeiras já marcadas no tabuleiro
+			int nBandeiras = ContaBandeiras(Tabuleiro);
+			// Verifica se atingiu a pontuação máxima e marcou todas as bandeiras
+			if (pontos == ((_Width_ * _Height_) - _QtBombas_) && _QtBombas_ == nBandeiras) {
+				// Mostramos o tabuleiro e finalizamos o jogo
+				MostraTabuleiro(Tabuleiro, TabVisual);
+				printf("Final de jogo ! Voce venceu por pontuacao maxima, sua pontuacao: %d\n", pontos);
+				break;
+			}
+			// Se atingiu pontuação mas não marcou todas as bombas com bandeiras
+			else if (pontos == ((_Width_ * _Height_) - _QtBombas_) && _QtBombas_ != nBandeiras) {
+				// Exibimos uma dica para o jogador
+				printf("Voce deve marcar todas as bombas com bandeiras para vencer o jogo !\n");
+			}
+			// Mandamos mostrar o campo e pontuação atual
 			printf("Campo Minado:\n");
 			MostraTabuleiro(Tabuleiro, TabVisual);
 			printf("Pontuacao atual: %d\n", pontos);
 		}
 	}
 
-	// Iremos utilizar o jogo como avaliação para o semestre
-	// Implementar abertura de célula de contagem, que não é bomba.
-	// Implementar abertura de célula que é bomba -> final de jogo
-
-	//printf("\nFinal de jogo, total de pontos: %d\n", pontos);
-
+	// TODO, ao marcar todas as bandeiras corretamente nos locais das bombas, 
+	// finalizar o jogo por pontuação máxima com 100 pontos.
 }
